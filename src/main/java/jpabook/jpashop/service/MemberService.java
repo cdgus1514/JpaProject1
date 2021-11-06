28c2358f5365eb2bce65bd01a840afece6aa5c80
choidh <cdgus1514@gmail.com>
2021-11-06 13:26:49 +0900

    회원 도메인 개발
    - 회원 리포지토리
    - 회원 서비스
    - 회원기능 테스트(테스트 코드 작성 및 테스트 환경설정)

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,71 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    //@Autowired
+    private final MemberRepository memberRepository;
+
+//    /**
+//     * 생성자 인젝션
+//     * 생성자가 1개만 있으면 알아서 인젝션 해줌
+//     * @param memberRepository
+//     */
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    // @RequiredArgsConstructor : final 필드만 가지고 생성자 만들어줌
+
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 중복회원 검증
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    // 검증을 해도 동시에 같은 이름을 등록하는 경우 검증 안될 수 있음 > name(유니크 제약조건 설정 필요)
+    private void validateDuplicateMember(Member member) {
+        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+    /**
+     * 회원 전체조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+
+    /**
+     * 회원조회
+     * @param memberId
+     * @return
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
