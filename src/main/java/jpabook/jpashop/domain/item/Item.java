ad3f81bad99cc5a45c5bfbe7e38ae983474e250c
choidh <cdgus1514@gmail.com>
2021-11-06 20:23:36 +0900

    상품 도메인 개발
    - 상품 엔티티(비즈니스 로직)
    - 회원 리포지토리
    - 회원 서비스

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,31 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // ==비지니스 로직== //
+    /*
+    *  도메인 주도 설계 시 엔티티 자체에서 해결할 수 있는 것은 처리하기
+    *  setter 대신 사용
+    */
+
+    /**
+     * 제고증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 취소 시 제고원복
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
