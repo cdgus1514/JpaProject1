1e9aaaf1c247f7b4d895827fdf66abaf7ff998fc
choidh <cdgus1514@gmail.com>
2021-11-09 21:59:23 +0900

    웹계층 개발 및 변경감지(수정)
    - 타임리프
    - 회원등록, 조회
    - 상품등록, 조회, 수정
    - 상품주문, 조회, 취소

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        // 같은 엔티티 조회
+        Item findItem = itemRepository.findOne(itemId);
+
+        // 데이터 수정
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
 
